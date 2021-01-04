cdb7b297eb9ddeed129dc2ba98c4c6b9aaa1d78f
matthew-lam <matthewlam35@hotmail.co.uk>
2021-01-04 01:51:27 +0000

    added more utilities stuff

--- src/scripts/data_utils.py
+++ src/scripts/data_utils.py
@@ -1,3 +1,16 @@
+import get_images as dbc_images
+
+def is_correct_dimensions(image):
+  """
+  Checks if the image has the correct dimensions to be able to be fed into the CNN properly.
+  args: image -- (string) directory of image.
+  returns: (boolean) -- True or False, if image has correct dimensions.
+  """
+  return False
+
+def pad_images_for_correct_dimensions(image):
+  pass
+
 def resize_image_with_same_aspect_ratio(image_src):
   """
   Upscales or downscales image if it can fit within the same aspect ratio.
--- src/scripts/get_images.py
+++ src/scripts/get_images.py
@@ -1,7 +1,9 @@
 # Get random images from dog.ceo API
 # This can be used as an input in a trained neural network
-
+import os
 import requests
+
+dog_pic_dir = os.getcwd() + '/dog_pics/'
 
 def get_random_dog_images(quantity=20):
   """ Gets random dog images using a GET request.
@@ -16,15 +18,43 @@ def check_url_contains_valid_image(image_url):
   """
   Checks if a url has a valid image.
   It does this by checking the media type in the header of the http request response.
-  args: image_url -- URL with what we think is an image.
+  args: image_url -- (string) URL with what we think is an image.
   returns: (boolean) -- True OR False, if url contains a valid image.
   """
   valid_formats = ("image/png", "image/jpeg", "image/jpg")
-  url_contents = requests.get(image_url)
-  if url_contents.headers["content-type"] in valid_formats:
+  r = requests.get(image_url)
+  if r.headers["content-type"] in valid_formats:
       return True
   return False
 
 
 def check_dir_contains_valid_image(image_src):
+  """
+  Checks if an file from a local directory is a valid image.
+  args: image_src -- (string) directory of file
+  returns: (boolean) -- True OR False, if file is a valid image.
+  """
   pass
+
+def save_image_locally(url, dir):
+  """
+  Gets image file from URL and saves it into a directory with the base name of the file as it is. (Checks if image first)
+  args:
+    url -- (string) URL of file
+    dir -- (string) target directory where file is saved
+  """
+  if check_url_contains_valid_image(url):
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+      file_name = os.path.basename(url)
+      with open(dir + file_name, 'wb') as f:
+        for chunk in r:
+          f.write(chunk)
+
+# Define `main()` function
+# def main():
+#   save_image_locally('https://images.dog.ceo/breeds/chow/n02112137_1251.jpg', dog_pic_dir)
+
+# # Execute `main()` function
+# if __name__ == '__main__':
+#     main()

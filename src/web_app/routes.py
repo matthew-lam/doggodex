861495e39457632b348a68d49883ca08fcd0ea9c
matthew-lam <matthewlam35@hotmail.co.uk>
2021-04-21 21:22:37 +0100

    routing fixed and able to store and load pics between routes.

--- src/web_app/routes.py
+++ src/web_app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, request, session, url_for, current_app as app
+from flask import Blueprint, render_template, redirect, session, url_for, current_app as app
 from werkzeug.utils import secure_filename
 import os
 
@@ -16,13 +16,20 @@ def index():
     # Upload dog image -- for uploading a file that contains an image (presumably, of a dog)
     form = FileUploadForm()
     if form.validate_on_submit():
+        file_name = secure_filename(form.file.data.filename)
+        form.file.data.save(bp.static_folder + '/' + file_name)
+        session['image'] = file_name
+        # 1. Predict here and then save it in a session.
         return redirect(url_for("bp.updog"))
     return render_template('index.html', form=form)
 
 
 @bp.route("/updog", methods=['GET'])
 def updog():
-    return render_template('updog.html')
+    # 2. Extract the prediction from the session and pass it into the template to render too.
+    # 3. Write a callback to delete the files and execute in the template
+    image = session['image']
+    return render_template('updog.html', image=image)
 
 
 @bp.route("/motivation")
